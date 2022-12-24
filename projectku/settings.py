d276d1b431fcc9668396d40b53fddaa8b1e92dcd
Thoriq Ikhwan Alfawas <2011102441142@umkt.ac.id>
2022-12-24 16:11:01 +0800

    update requestments.txt.

--- projectku/settings.py
+++ projectku/settings.py
@@ -158,8 +158,12 @@ WSGI_APPLICATION = 'projectku.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306',
+        'NAME': 'berita',
+        'USER' : 'root',
+        'PASSWORD' : '',
     }
 }
 
